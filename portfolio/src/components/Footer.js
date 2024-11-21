db6338e9f30f7e21c28c0141d5f664df966ae21c
Tanisha <Tanisharampersad24@gmail.com>
2024-11-21 18:48:15 +0200

    updated project text color

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,3 +1,13 @@
+import { useEffect } from "react"
+
 export default function Footer() {
-    
+    useEffect(() => {
+
+    })
+         
+        return(
+            <>
+                <h1>Hello there</h1>
+            </>
+        )
 }
