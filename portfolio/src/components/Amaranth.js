3cb8937ded4f649cb59f58ed1d68a4082305785e
Tanisha <Tanisharampersad24@gmail.com>
2024-11-29 16:54:27 +0200

    Edited amaranth heading

--- portfolio/src/components/Amaranth.js
+++ portfolio/src/components/Amaranth.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { HashLink as Link} from 'react-router-hash-link';
 //import gsap from 'gsap';
 import './Amaranth.css'
+import arrow from '../images/right-arrow (1).png'
 //import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
@@ -15,6 +16,25 @@ export default function Amaranth() {
                 <Link>
                     <h1>Back to Home</h1>
                 </Link>
+            </section>
+
+
+            <section className="heading">
+                <h2>Amaranth</h2>
+
+                <div className="role">
+                    <p className="role-heading">Role/Services</p>
+
+                    <div className="horizontal-line">
+                        <hr/>
+                    </div>
+                    <p className="role-text">Design and Development</p>
+                </div>
+
+
+                <div className="visit-site">
+                    <div className="site">Live Site<img className="arrow" src={arrow}alt='arrow'/></div>
+                </div>
             </section>
         
         </div>
